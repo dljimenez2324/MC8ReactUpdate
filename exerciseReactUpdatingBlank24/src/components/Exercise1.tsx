1336999290c12a08e4b92427e167ce78b6183586
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-22 22:41:46 -0700

    accidentally added node modules in two locations

--- exerciseReactUpdatingBlank24/src/components/Exercise1.tsx
+++ exerciseReactUpdatingBlank24/src/components/Exercise1.tsx
@@ -12,13 +12,33 @@ const Exercise1 = () => {
     });
 
     const handleClick = () => {
-        // setGame game.player.name = 'Aaron';
+        // game.player.name = 'Aaron';
+
+        setGame({
+          ...game,
+          player:{...game.player, name:'Aaron'}
+        })
        
     }
+
+    // to change it back to David make another helper function structured the same way but with the string as 'David'
+    const handleClickBack = () => {
+      setGame({
+        ...game,
+        player:{...game.player, name:'David'}
+      })
+    }
+
   return (
-    <div>
-        <button onClick={handleClick}>Click to Change Name</button>
-        <div>{game.player.name}</div>
+    <div className="exercise1">
+        <h3>Exercise 1</h3>
+        <div className="mx-2 p-2">{game.player.name}</div>
+        <div>
+
+        <button onClick={handleClick} className="col-2 btn btn-primary m-1">Click to Change Player's Name</button>
+        <button onClick={handleClickBack} className="col-3 btn btn-primary m-1">Click to Change Player's Name Back</button>
+        </div>
+        <br></br>
     </div>
   )
 }
